b205f79620e295ca8d2295aa4f43bd9e8aadd7da
atulkumarsingh <atuls611btech2025@kccitm.edu.in>
2022-07-16 21:25:59 +0530

    updated

---
+++ palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,Y=0,Z;
+    printf("enter the number");
+    scanf("%d",&n);
+    Z=n;
+    while (n>0);
+    {
+        Y=n%10;
+        Y=(Y*10)+r;
+        n=n/10;
+    }
+    if(Z==Y)
+    printf("palindrome number ");
+    else
+    printf(" not a palindrome number");
+    return 0;
+}
